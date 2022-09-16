2dd416a21dc7daf99b558fd75565281628a2eca6
alexy <alexy.lefevre@amue.fr>
2022-09-16 15:41:20 +0200

    refacto move services

--- src/app/services/tests/hero.service.spec.ts
+++ src/app/services/tests/hero.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { InMemoryDataService } from './in-memory-data.service';
+import { HeroService } from '../hero.service';
 
-describe('InMemoryDataService', () => {
-  let service: InMemoryDataService;
+describe('HeroService', () => {
+  let service: HeroService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(InMemoryDataService);
+    service = TestBed.inject(HeroService);
   });
 
   it('should be created', () => {
